f3924d157b87f990d8c2278e3f67b6a2b17ac828
EvgenS-it <evgens.it101@gmail.com>
2024-09-06 16:11:29 +0100

    delete old comments

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -66,22 +66,7 @@ const contactsSlice = createSlice({
       }),
 });
 
-// export const selectContacts = state => state.contacts.contacts.items;
-// export const selectContactsLoading = state => state.contacts.contacts.loading;
-// export const selectContactsError = state => state.contacts.contacts.error;
-
-// // compound selector, which includes 2 simple selectors
-// export const selectFilteredContacts = createSelector(
-//   [selectContacts, selectNameFilter],
-//   (contacts, filter) => {
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//   }
-// );
-
 export const contactsReducer = contactsSlice.reducer;
-// export const { addContact, deleteContact } = contactsSlice.actions;
 
 // Vanila redux logic
 // export const contactsReducer = (state = INITIAL_STATE, action) => {
