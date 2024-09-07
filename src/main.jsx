a5d7c7e7a421362b7620bca1261ad39d88c56215
EvgenS-it <evgens.it101@gmail.com>
2024-09-07 17:35:41 +0100

    router installed

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import App from './components/App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
 // import { persistor, store } from './redux/store.js';
 // import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        {/* <PersistGate persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,9 @@
+// const INITIAL_STATE = {
+//   user: {
+//     name: null,
+//     email: null,
+//   },
+//   token: null,
+//   isLoggedIn: false,
+//   isRefreshing: false,
+// };
